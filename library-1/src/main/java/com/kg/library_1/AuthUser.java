427d787497653a626a8fcc7b80ebd9e538793631
kdk <kimdk1206@naver.com>
2024-02-02 15:30:28 +0900

    test

--- library-1/src/main/java/com/kg/library_1/AuthUser.java
+++ library-1/src/main/java/com/kg/library_1/AuthUser.java
@@ -2,7 +2,7 @@ package com.kg.library_1;
 
 import java.io.Serializable;
 
-public class AuthUser implements Serializable {
+public class AuthUser implements Serializable { //redis session에서 직렬화 하여 사용 할 수 있게 인터페이스를 구현한 클래스 
 
 	private String id;
 
